65ba9f1f023e18af82cfd6028442351b42f56d36
Esther Heid <esther_heid@web.de>
2023-07-14 11:09:42 +0200

    added benchmarks

---
+++ scripts/pcba_scaffold.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+chemprop_dir=../../../chemprop  # location of chemprop directory, CHANGE ME
+
+results_dir=results_pcba_scaffold
+train_path=../data/pcba_scaffold/train.csv
+val_path=../data/pcba_scaffold/val.csv
+test_path=../data/pcba_scaffold/test.csv
+
+#Hyperparameter optimization
+python $chemprop_dir/hyperparameter_optimization.py \
+--dataset_type classification \
+--data_path $train_path \
+--separate_val_path $val_path \
+--separate_test_path $val_path \
+--num_iters 30 \
+--epochs 50 \
+--aggregation norm \
+--search_parameter_keywords depth ffn_num_layers  hidden_size ffn_hidden_size dropout \
+--config_save_path $results_dir/config.json \
+--hyperopt_checkpoint_dir $results_dir \
+--log_dir $results_dir 
+
+#Training with optimized hyperparameters
+python $chemprop_dir/train.py \
+--dataset_type classification \
+--data_path $train_path \
+--separate_val_path $val_path \
+--separate_test_path $test_path \
+--epochs 50 \
+--aggregation norm \
+--config_path $results_dir/config.json \
+--save_dir $results_dir \
+--ensemble_size 5 \
+--save_preds \
+--extra_metrics prc-auc
+
---
+++ scripts/qm9_multitask.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+chemprop_dir=../../../chemprop  # location of chemprop directory, CHANGE ME
+
+results_dir=results_qm9_multitask
+train_path=../data/qm9/train.csv
+val_path=../data/qm9/val.csv
+test_path=../data/qm9/test.csv
+
+#Hyperparameter optimization
+python $chemprop_dir/hyperparameter_optimization.py \
+--dataset_type regression \
+--data_path $train_path \
+--separate_val_path $val_path \
+--separate_test_path $val_path \
+--num_iters 30 \
+--epochs 50 \
+--aggregation norm \
+--search_parameter_keywords depth ffn_num_layers  hidden_size ffn_hidden_size dropout \
+--config_save_path $results_dir/config.json \
+--hyperopt_checkpoint_dir $results_dir \
+--log_dir $results_dir 
+
+#Training with optimized hyperparameters
+python $chemprop_dir/train.py \
+--dataset_type regression \
+--data_path $train_path \
+--separate_val_path $val_path \
+--separate_test_path $test_path \
+--epochs 50 \
+--aggregation norm \
+--config_path $results_dir/config.json \
+--save_dir $results_dir \
+--ensemble_size 5 \
+--save_preds \
+--extra_metrics mae \
+--show_individual_scores
